96272daca1f4db5735f05f0798e25d8da90c6d67
Jamil Omar <jfalconi63@hotmail.com>
2019-07-02 15:12:49 -0400

    fix: labshare template with new webpack server

--- ui/main.ts
+++ ui/main.ts
@@ -1,5 +1,10 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {NewappModule} from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+if(ENABLE_PROD_MODE===true){
+  enableProdMode();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(NewappModule)
