2a7b65d21f0967a01c6371179a7e71023d9a8d80
Jamil Omar <jfalconi63@hotmail.com>
2019-07-09 01:20:13 -0400

    fix: template

--- ui/main.ts
+++ ui/main.ts
@@ -1,8 +1,8 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {NewappModule} from './app/app.module';
-import { enableProdMode } from '@angular/core';
+import {enableProdMode} from '@angular/core';
 
-if(ENABLE_PROD_MODE===true){
+if (ENABLE_PROD_MODE === true) {
   enableProdMode();
 }
 
